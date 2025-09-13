e5fc47c4b3f35dc6b8c94868aaa6e83199b25b02
Parveen Kumar <parveenkumar66360@gmail.com>
2025-09-13 12:30:05 +0000

    new

---
+++ ques_practice/ques1.py
@@ -0,0 +1,7 @@
+'''Accept name from a user
+ Accept age from the user and print it'''
+
+name=input("enter your name: ")
+age=input("enter your age: ")
+
+print("My name is {} and i am {} years old. ".format(name,age))
