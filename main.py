7e184ba85592c713164f4b31972634300f63fa5c
Parveen Kumar <parveenkumar66360@gmail.com>
2025-09-16 17:37:59 +0000

    change

--- main.py
+++ main.py
@@ -552,4 +552,322 @@ b = 12
 #     content=f.read()
 #     print(content)
 
+'''OOPS'''
 
+# class Factory:
+#     a = 12 # class attribute 
+
+#     def hello(self): #method
+#         print("how are you")
+
+#directly accessing atribute and mehod using the class
+# Factory().hello()        #call method
+# print(Factory().a)       #access atribute 
+
+# obj = Factory()
+
+# obj2 = Factory()
+
+
+'''Dunder method ---constructor--self'''
+# class Factory:
+#     def __init__(self,material,zips,pockets): 
+#         print(self)                     #print the postion of objext 
+#         self.material = material      #instance aatribute
+#         self.zips = zips
+#         self.pockets = pockets
+    
+#     def show(self):
+#         print(f"your object details are {self.material}, {self.pockets},{self.zips} ")
+    
+
+
+# reebok = Factory("leather",3,2)      #print position of reebok on self
+
+# campus = Factory("nylon",3,3)
+
+# reebok.show()
+
+
+
+# class Animal:
+#     name = "lion" #class attribute
+    
+#     def __init__(self,age):
+#         self.age = age #instance attribute
+    
+#     def show(self):  #instance method
+#         print(f"how are you your agger is {self.age}")
+    
+#     @classmethod
+#     def hello(cls):
+#         print(f"how are you brother {cls.age}")
+
+#     @staticmethod
+#     def static():
+#         print("how are you")
+
+   
+
+# obj = Animal(12)
+# obj.show()
+# obj.hello()
+# obj.statis()
+
+
+'''Inheritance'''
+
+# class Factorymumbai: #parent class / superclass
+#     a = "I am an attribute mentioned inside Factory"
+#     def hello(self):
+#         print("hello I am a method mentioned inside Factory")
+
+# class Factorypune(Factorymumbai):   #child class /subclass
+#     pass
+
+# obj = Factorymumbai()
+
+# obj2 = Factorypune()
+
+# obj2.hello()
+
+'''Constructor in inheritance'''
+
+# class Animal:
+#     def __init__(self,name):
+#         self.name = name
+    
+#     def show(self):
+#         print(f"hello your name is {self.name}")
+    
+
+# class Human(Animal):
+#     def __init__(self, name,age):
+#         super().__init__(name)
+#         self.age = age
+    
+#     def show(self):
+#         print(f"hello your name is {self.name},{self.age}")
+
+
+# animal1 = Animal("lion")
+# person1 = Human("akarsh",23)
+
+# animal1.show()
+
+'''multiple inheritance'''
+# class Animal:
+#     def __init__(self,name):
+#         pass
+
+# class Human:
+#     def __init__(self,name,age):
+#         pass
+
+# class Robots(Human,Animal):    #first serve first come
+#     name3 = "charli123"
+
+# obj = Robots()       #argument of human parameter
+
+'''multilevel inheritance'''
+# class Factory:
+#     def __init__(self,material,zips):
+#         self.material = material
+#         self.zips = zips 
+    
+
+# class BhopalFactory(Factory):
+#     def __init__(self, material, zips,color):
+#         super().__init__(material, zips)
+#         self.color = color
+    
+# class Punefactory(BhopalFactory):
+#     def __init__(self, material, zips, color,pockets):
+#         super().__init__(material, zips, color)
+#         self.pockets = pockets
+
+# obj=Punefactory()
+
+
+'''Polymorpism'''
+
+#overiding
+
+# class Animal:
+#     def show(self):
+#         print("hello I am akarsh")
+    
+
+
+# class Human(Animal):
+#     def show(self):
+#         print("how are you")
+
+# obj = Human()
+# obj.show()
+
+'''Duckk typing'''
+
+# class Animal:
+#     def show(self):
+#         print("I am showing ")
+
+# class Human:
+#     def show(self):
+#         print("hello I am also showing ")
+
+# obj = Animal()
+# obj2 = Human()
+
+# obj.show()
+# obj2.show()
+
+'''Encapsulation'''
+
+# class Demo():
+#     def __init__(self):
+#         self.name="Public member"     #public
+#         self._age=21                  #protected
+#         self.__salary=30000           #private
+
+#     def show(self):
+#         print("Inside the class")
+#         print("Public:",self.name)
+#         print("Protected:",self.age)
+#         print("Private:",self.salary)
+
+# obj=Demo()
+# obj.show()
+
+
+#private
+# class Factory:
+#     __a = "pune"  
+
+#     def show(self):
+#         print(Factory.__a) #access in class
+
+
+# obj = Factory() 
+
+# obj.show()  #pune but not changed
+
+'''Abstraction'''
+
+# from abc import ABC, abstractmethod
+
+# class abstract(ABC):
+#     @abstractmethod
+#     def perimeter(self):
+#         pass 
+    
+#     @abstractmethod
+#     def area(self):
+#         pass
+
+# class Square(abstract):    #rule followed by abstract class
+#     def __init__(self,side):
+#         self.side = side
+
+#     def perimeter(self):
+#         print("i have created")
+    
+#     def area(self):
+#         print("I have created this ")
+
+# class Circle(abstract):
+#     def __init__(self,radius):
+#         self.radius = radius
+    
+#     def perimeter(self):
+#         print("i have created")
+    
+#     def area(self):
+#         print("I have created this ")
+
+# obj = Circle(7)
+# obj2 = Square(12)
+
+
+'''Dunder method'''
+# class Animal:
+#     def __init__(self,name,age):
+#         self.name = name
+#         self.age = age
+    
+#     def __str__(self):
+#         return f"hello how are you and your name is {self.name}"
+
+#     def __add__(self,other):
+#         sum = 0
+#         for i in other:
+#             sum = sum + i.age
+
+#         return f"your sum of ages are {self.age + sum}"
+
+# obj = Animal("lion",12)
+# obj2 = Animal("dolphin",14)
+# obj3 = Animal("tiger",34)
+
+# print(obj + (obj2,obj3))
+
+
+'''Decorator'''
+# class Animal:
+#     @property
+#     def show(self):
+#         print("hello how are you")
+    
+# obj = Animal()
+
+# obj.show
+
+
+
+# def decorate(func):
+#     def wrapper(a,b):
+#         print("the addition to your numbers are ")
+#         func(a,b)
+#         print("thankyou I hope you liked it ")
+#     return wrapper
+
+
+# @decorate
+# def addition(a,b):
+#     print(f"your total is {a + b} ")
+
+# addition(12,67)
+
+''' *args and **kwargs'''
+
+# def addition(*args):
+#     sum=0
+#     for i in args:
+#         sum=sum+i
+
+#     print(sum)
+
+# addition(322,3,32,3,3,3,2,33,2,32,)        
+
+
+# def information(**kwargs):
+#     print("your informations is\n ")
+#     for i in kwargs:
+#         print(f"{i} : {kwargs[i]}")
+
+# information(name="PArveen",age=21,domain="AI/Ml")        
+
+
+# def decorate(func):
+#     def wrapper(*args,**kwargs):
+#         print("the addition to your numbers are ")
+#         func(*args,**kwargs)
+#         print("thankyou I hope you liked it ")
+#     return wrapper
+
+
+# @decorate
+# def addition(a,b):
+#     print(f"your total is {a + b} ")
+
+# addition(12,67)
